83a5c03b5a9230024c3ae4577ef4fdf55fe8db20
cathy <cathy.siwatala@gmail.com>
2021-12-20 17:09:19 +0100

    Creation tables 'prestation'

--- prestations/models.py
+++ prestations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Prestation(models.Model):
+    nomPrestation = models.CharField(max_length=200)
+    heureArrivee = models.TimeField
+    heureDepart = models.TimeField
