9b582350871c1ef303c002d76cdd363b7ef24813
cathy <cathy.siwatala@gmail.com>
2021-12-20 17:05:31 +0100

    Creation tables 'client' et 'employé'

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    prenomClient = models.CharField(max_length=200)
+    nomClient = models.CharField(max_length=200)
+    ageClient = models.IntegerField
+    emailClient = models.CharField(max_length=200)
+    adresseClient = models.CharField(max_length=600)
+
--- employes/models.py
+++ employes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Employe(models.Model):
+    prenomEmploye = models.CharField(max_length=200)
+    nomEmploye = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
