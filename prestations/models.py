601204e036488f31fa47f54d9b72a54573b31fae
cathy <cathy.siwatala@gmail.com>
2021-12-21 11:28:02 +0100

    Modification table 'prestation'

--- prestations/models.py
+++ prestations/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from clients.models import Client
+from employes.models import Employe
+
 
 # Create your models here.
 class Prestation(models.Model):
     nomPrestation = models.CharField(max_length=200)
-    heureArrivee = models.TimeField
-    heureDepart = models.TimeField
+    heureArrivee = models.TimeField(auto_now_add=True, null=True)
+    heureDepart = models.TimeField(auto_now_add=True, null=True)
+    employe =models.ForeignKey(Employe,null=True, on_delete=models.SET_NULL)
+    client = models.ForeignKey(Client, null=True, on_delete=models.SET_NULL)
+
