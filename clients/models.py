a33321ad9e1b9cdc324c013de4726e142daefba4
cathy <cathy.siwatala@gmail.com>
2021-12-21 11:15:40 +0100

    Modification tables

--- clients/models.py
+++ clients/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Client(models.Model):
-    prenomClient = models.CharField(max_length=200)
-    nomClient = models.CharField(max_length=200)
-    ageClient = models.IntegerField
-    emailClient = models.CharField(max_length=200)
-    adresseClient = models.CharField(max_length=600)
+    prenomClient = models.CharField(max_length=200, null=True)
+    nomClient = models.CharField(max_length=200, null=True)
+    ageClient = models.IntegerField(null=True)
+    emailClient = models.CharField(max_length=200, null=True)
+    adresseClient = models.CharField(max_length=600, null=True)
 
--- employes/models.py
+++ employes/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Employe(models.Model):
-    prenomEmploye = models.CharField(max_length=200)
-    nomEmploye = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    prenomEmploye = models.CharField(max_length=200, null=True)
+    nomEmploye = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
